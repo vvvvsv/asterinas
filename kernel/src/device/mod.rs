4c16d08e7725b0ab8b40316a083e1b6bd56e0dd8
Wei Zhang <ruoyuan.zw@antgroup.com>
2025-04-23 07:23:40 +0800

    Add input device
    Signed-off-by: Wei Zhang <ruoyuan.zw@antgroup.com>

--- kernel/src/device/mod.rs
+++ kernel/src/device/mod.rs
@@ -8,6 +8,7 @@ mod shm;
 pub mod tty;
 mod urandom;
 mod zero;
+mod event;
 
 #[cfg(all(target_arch = "x86_64", feature = "cvm_guest"))]
 mod tdxguest;
@@ -16,12 +17,14 @@ pub use fb::Fb;
 pub use pty::{new_pty_pair, PtyMaster, PtySlave};
 pub use random::Random;
 pub use urandom::Urandom;
+pub use event::EventDevice;
 
 use self::tty::get_n_tty;
 use crate::{
     fs::device::{add_node, Device, DeviceId, DeviceType},
     prelude::*,
 };
+use alloc::format;
 
 /// Init the device node in fs, must be called after mounting rootfs.
 pub fn init() -> Result<()> {
@@ -46,6 +49,15 @@ pub fn init() -> Result<()> {
     add_node(fb, "fb0")?;
     pty::init()?;
     shm::init()?;
+
+    // Dynamically create EventDevices for each InputDevice
+    for (index, (device_name, input_device)) in aster_input::all_devices().iter().enumerate() {
+        let event_device = Arc::new(event::EventDevice::new(index, input_device.clone()));
+        let path = format!("input/event{}", index);
+        add_node(event_device, &path)?;
+        println!("Added EventDevice for InputDevice '{}' at '{}'", device_name, path);
+    }
+
     Ok(())
 }
 
