e92d49ab78f0b6a6957cba00d1c1594545b6320f
Zhe Tang <tangzh6101@gmail.com>
2025-05-09 08:26:08 +0000

    Improve the fb benchmark to test munmap

--- test/apps/fb/fb-test.c
+++ test/apps/fb/fb-test.c
@@ -10,67 +10,80 @@
 #define PUTVSCREENINFO 0x4601
 #define GETFSCREENINFO 0x4602
 
-// struct FbVarScreenInfo {
-//     unsigned int xres;
-//     unsigned int yres;
-//     unsigned int xres_virtual;
-//     unsigned int yres_virtual;
-//     unsigned int bits_per_pixel;
-// };
+unsigned int colors[] = {
+	// RGB value for colors (assuming 32-bit color depth)
+	0xFF0000, // Red
+	0x00FF00, // Green
+	0x0000FF, // Blue
+	0xFFFF00, // Yellow
+	0xFF00FF, // Magenta
+	0x00FFFF, // Cyan
+	0xFFFFFF, // White
+	0x000000 // Black
+};
+const unsigned int color_count = sizeof(colors) / sizeof(colors[0]);
 
-// struct FbFixScreenInfo {
-//     unsigned long smem_start;
-//     unsigned long smem_len;
-//     unsigned long line_length;
-// };
+int main()
+{
+	int fb_fd = open("/dev/fb0", O_RDWR);
+	if (fb_fd < 0) {
+		perror("Failed to open /dev/fb0");
+		return 1;
+	}
 
+	struct fb_var_screeninfo var_info;
+	if (ioctl(fb_fd, GETVSCREENINFO, &var_info) == 0) {
+		printf("Framebuffer resolution: %ux%u, virtual: %ux%u, bpp: %u\n",
+		       var_info.xres, var_info.yres, var_info.xres_virtual,
+		       var_info.yres_virtual, var_info.bits_per_pixel);
+	} else {
+		perror("GETVSCREENINFO ioctl failed");
+	}
 
-int main() {
-    int fb_fd = open("/dev/fb0", O_RDWR);
-    if (fb_fd < 0) {
-        perror("Failed to open /dev/fb0");
-        return 1;
-    }
+	struct fb_fix_screeninfo fix_info;
+	if (ioctl(fb_fd, GETFSCREENINFO, &fix_info) == 0) {
+		printf("Framebuffer memory: start=0x%lx, length=%u bytes, line length=%u bytes\n",
+		       fix_info.smem_start, fix_info.smem_len,
+		       fix_info.line_length);
+	} else {
+		perror("GETFSCREENINFO ioctl failed");
+	}
 
-    struct fb_var_screeninfo var_info;
-    if (ioctl(fb_fd, GETVSCREENINFO, &var_info) == 0) {
-        printf("Framebuffer resolution: %ux%u, virtual: %ux%u, bpp: %u\n",
-               var_info.xres, var_info.yres, var_info.xres_virtual, var_info.yres_virtual, var_info.bits_per_pixel);
-    } else {
-        perror("GETVSCREENINFO ioctl failed");
-    }
+	for (unsigned int i = 0; i < color_count; i++) {
+		void *fb_mem = mmap(NULL, fix_info.smem_len,
+				    PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd,
+				    0);
+		if (fb_mem == MAP_FAILED) {
+			perror("Failed to mmap framebuffer memory");
+		} else {
+			printf("Framebuffer memory mapped at address %p\n",
+			       fb_mem);
 
-    struct fb_fix_screeninfo fix_info;
-    if (ioctl(fb_fd, GETFSCREENINFO, &fix_info) == 0) {
-        printf("Framebuffer memory: start=0x%lx, length=%u bytes, line length=%u bytes\n",
-               fix_info.smem_start, fix_info.smem_len, fix_info.line_length);
-    } else {
-        perror("GETFSCREENINFO ioctl failed");
-    }
-    void *fb_mem = mmap(NULL, fix_info.smem_len, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd, 0);
-    if (fb_mem == MAP_FAILED) {
-        perror("Failed to mmap framebuffer memory");
-    } else {
-        printf("Framebuffer memory mapped at address %p\n", fb_mem);
+			//Fill the framebuffer with different colors
+			if (var_info.bits_per_pixel == 32) {
+				unsigned int *pixel = (unsigned int *)fb_mem;
+				for (unsigned int y = 0; y < var_info.yres;
+				     y++) {
+					for (unsigned int x = 0;
+					     x < var_info.xres; x++) {
+						pixel[y * (fix_info.line_length /
+							   4) +
+						      x] = colors[i];
+					}
+				}
+			} else {
+				fprintf(stderr,
+					"Unsupported bits per pixel: %u\n",
+					var_info.bits_per_pixel);
+			}
+			sleep(3);
+			//Unmap the framebuffer memory
+			if (munmap(fb_mem, fix_info.smem_len) < 0) {
+				perror("Failed to munmap framebuffer memory");
+			}
+		}
+	}
 
-        //Fill the framebuffer with red color
-        unsigned int red_color = 0xFF0000; // RGB value for red (assuming 32-bit color depth)
-        if (var_info.bits_per_pixel == 32) {
-            unsigned int *pixel = (unsigned int *)fb_mem;
-            for (unsigned int y = 0; y < var_info.yres; y++) {
-                for (unsigned int x = 0; x < var_info.xres; x++) {
-                    pixel[y * (fix_info.line_length / 4) + x] = red_color;
-                }
-            }
-        } else {
-            fprintf(stderr, "Unsupported bits per pixel: %u\n", var_info.bits_per_pixel);
-        }
-        sleep(5);
-        //Unmap the framebuffer memory
-        if (munmap(fb_mem, fix_info.smem_len) < 0) {
-            perror("Failed to munmap framebuffer memory");
-        }
-    }
-    close(fb_fd);
-    return 0;
+	close(fb_fd);
+	return 0;
 }
