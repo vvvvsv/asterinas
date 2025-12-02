7ffc393e26329583937a7b9cbc7c65246b46430b
vvsv <vvsv_7169@163.com>
2025-12-02 10:01:50 +0000

    nix-shell: Log in as root

--- distro/modules/core.nix
+++ distro/modules/core.nix
@@ -31,8 +31,8 @@ in {
     echo "Executing postBootCommands..."
     rm -rf /etc/resolv.conf
     ln -s ${resolv-conf} /etc/resolv.conf
-    PATH=$PATH:/nix/var/nix/profiles/system/sw/bin:~/.nix-profile/bin
     if [ "${builtins.getEnv "NIXOS_DISABLE_SYSTEMD"}" = "true" ]; then
+      source /etc/profile
       ${builtins.getEnv "NIXOS_STAGE_2_INIT"}
     fi
   '';
