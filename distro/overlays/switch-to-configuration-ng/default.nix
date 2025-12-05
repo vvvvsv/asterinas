4f5438999e4910137c8ff4b23802ad935bff910c
vvsv <vvsv_7169@163.com>
2025-12-05 06:24:03 +0000

    nixos-rebuild: Bypass systemd

---
+++ distro/overlays/switch-to-configuration-ng/default.nix
@@ -0,0 +1,8 @@
+self: super:
+
+{
+  switch-to-configuration-ng = super.switch-to-configuration-ng.overrideAttrs
+    (oldAttrs: {
+      patches = (oldAttrs.patches or [ ]) ++ [ ./0001-Bypass-systemd.patch ];
+    });
+}
